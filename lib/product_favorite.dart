a90acfe6384170de7b611978f43ed4c68f8b4e86
ranapermatahati <ranapermata28@gmail.com>
2026-04-30 12:57:33 +0700

    dinda

--- lib/product_favorite.dart
+++ lib/product_favorite.dart
@@ -1,1 +1,141 @@
-// TODO Implement this library.
+ import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pertemuan9_praktikum_kelompok5/models/product.dart';
+import 'package:pertemuan9_praktikum_kelompok5/product_detail_page.dart';
+
+class FavoriteProductsScreen extends StatelessWidget {
+  const FavoriteProductsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Filter produk favorit (isFavourite = true)
+    final favoriteProducts = demoProducts.where((p) => p.isFavourite).toList();
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: const Text("Favorite")),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: favoriteProducts.isEmpty
+              ? const Center(
+                  child: Text(
+                    "No favorite products yet.",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                )
+              : GridView.builder(
+                  itemCount: favoriteProducts.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 200,
+                    childAspectRatio: 0.7,
+                    mainAxisSpacing: 20,
+                    crossAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) {
+                    final product = favoriteProducts[index];
+                    return ProductCard(
+                      product: product,
+                      onPress: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductDetailsScreen(product: product),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+// Widget ProductCard (sama seperti di home_page)
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    Key? key,
+    this.width = 140,
+    required this.product,
+    required this.onPress,
+  }) : super(key: key);
+
+  final double width;
+  final Product product;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: GestureDetector(
+        onTap: onPress,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AspectRatio(
+              aspectRatio: 1.02,
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF979797).withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Image.asset('assets/images/${product.images[0]}'),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              product.title,
+              style: Theme.of(context).textTheme.bodyMedium,
+              maxLines: 2,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "\$${product.price}",
+                  style: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFFF7643),
+                  ),
+                ),
+                InkWell(
+                  borderRadius: BorderRadius.circular(50),
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(6),
+                    height: 24,
+                    width: 24,
+                    decoration: BoxDecoration(
+                      color: product.isFavourite
+                          ? const Color(0xFFFF7643).withOpacity(0.15)
+                          : const Color(0xFF979797).withOpacity(0.1),
+                      shape: BoxShape.circle,
+                    ),
+                    child: SvgPicture.string(
+                      heartIcon,
+                      colorFilter: ColorFilter.mode(
+                        product.isFavourite
+                            ? const Color(0xFFFF4848)
+                            : const Color(0xFFDBDEE4),
+                        BlendMode.srcIn,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+const heartIcon = '''<svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16.5266 8.61383L9.27142 15.8877C9.12207 16.0374 8.87889 16.0374 8.72858 15.8877L1.47343 8.61383C0.523696 7.66069 0 6.39366 0 5.04505C0 3.69644 0.523696 2.42942 1.47343 1.47627C2.45572 0.492411 3.74438 0 5.03399 0C6.3236 0 7.61225 0.492411 8.59454 1.47627C8.81857 1.70088 9.18143 1.70088 9.40641 1.47627C11.3691 -0.491451 14.5629 -0.491451 16.5266 1.47627C17.4763 2.42846 18 3.69548 18 5.04505C18 6.39366 17.4763 7.66165 16.5266 8.61383Z" fill="#DBDEE4"/>
+</svg>''';
